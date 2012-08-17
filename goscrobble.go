9ad5897dec89d291c784bb648811600e87c8df39
Adam Flott <adam@npjh.com>
2012-08-17 14:43:02 -0400

    Format with "gofmt -tabs=false -tabwidth=4 goscrobble.go"

--- goscrobble.go
+++ goscrobble.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/xml"
-	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
-	"sort"
-	"strconv"
-	"strings"
-	"time"
+    "crypto/md5"
+    "encoding/xml"
+    "flag"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "os"
+    "sort"
+    "strconv"
+    "strings"
+    "time"
 )
 
 var api_key *string
@@ -22,90 +22,90 @@ var session_key *string
 var token *string
 
 type LFM struct {
-	Status string `xml:"status,attr"`
+    Status string `xml:"status,attr"`
 }
 
 func main() {
 
-	api_key = flag.String("api_key", "", "API Key")
-	api_secret = flag.String("api_secret", "", "API Secret")
-	session_key = flag.String("session", "", "Session")
-	token = flag.String("token", "", "Token")
+    api_key = flag.String("api_key", "", "API Key")
+    api_secret = flag.String("api_secret", "", "API Secret")
+    session_key = flag.String("session", "", "Session")
+    token = flag.String("token", "", "Token")
 
-	artist := flag.String("artist", "", "Track artist")
-	album := flag.String("album", "", "Track album")
-	title := flag.String("title", "", "Track title")
+    artist := flag.String("artist", "", "Track artist")
+    album := flag.String("album", "", "Track album")
+    title := flag.String("title", "", "Track title")
 
-	flag.Parse()
+    flag.Parse()
 
-	u, _ := url.Parse("http://ws.audioscrobbler.com/2.0/")
+    u, _ := url.Parse("http://ws.audioscrobbler.com/2.0/")
 
-	q := u.Query()
-	q.Set("method", "track.scrobble")
+    q := u.Query()
+    q.Set("method", "track.scrobble")
 
-	t := time.Now()
-	ts := int(t.Unix())
+    t := time.Now()
+    ts := int(t.Unix())
 
-	q.Set("token", *token)
-	q.Set("api_key", *api_key)
-	q.Set("sk", *session_key)
-	q.Set("artist", *artist)
-	q.Set("album", *album)
-	q.Set("track", *title)
-	q.Set("timestamp", strconv.Itoa(ts))
+    q.Set("token", *token)
+    q.Set("api_key", *api_key)
+    q.Set("sk", *session_key)
+    q.Set("artist", *artist)
+    q.Set("album", *album)
+    q.Set("track", *title)
+    q.Set("timestamp", strconv.Itoa(ts))
 
-	s := make_api_sig(q)
+    s := make_api_sig(q)
 
-	q.Set("api_sig", s)
+    q.Set("api_sig", s)
 
-	u.RawQuery = q.Encode()
+    u.RawQuery = q.Encode()
 
-	resp, err := http.Post(u.String(), "", strings.NewReader(q.Encode()))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+    resp, err := http.Post(u.String(), "", strings.NewReader(q.Encode()))
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
+    defer resp.Body.Close()
+    body, _ := ioutil.ReadAll(resp.Body)
 
-	v := LFM{}
+    v := LFM{}
 
-	err = xml.Unmarshal([]byte(body), &v)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		os.Exit(2)
-	}
+    err = xml.Unmarshal([]byte(body), &v)
+    if err != nil {
+        fmt.Printf("error: %v", err)
+        os.Exit(2)
+    }
 
-	if v.Status == "ok" {
-		os.Exit(0)
-	} else {
-		os.Exit(3)
-	}
+    if v.Status == "ok" {
+        os.Exit(0)
+    } else {
+        os.Exit(3)
+    }
 }
 
 func make_api_sig(q url.Values) string {
 
-	mk := make([]string, len(q))
-	i := 0
-	for k, _ := range q {
-		mk[i] = k
-		i++
-	}
+    mk := make([]string, len(q))
+    i := 0
+    for k, _ := range q {
+        mk[i] = k
+        i++
+    }
 
-	sort.Strings(mk)
+    sort.Strings(mk)
 
-	h := md5.New()
+    h := md5.New()
 
-	var t string
-	for _, v2 := range mk {
-		t += v2
-		t += q.Get(v2)
-	}
+    var t string
+    for _, v2 := range mk {
+        t += v2
+        t += q.Get(v2)
+    }
 
-	t += *api_secret
+    t += *api_secret
 
-	io.WriteString(h, t)
+    io.WriteString(h, t)
 
-	return fmt.Sprintf("%x", h.Sum(nil))
+    return fmt.Sprintf("%x", h.Sum(nil))
 }
